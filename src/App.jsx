5510333e48a8adc73e0ee9776b6fe544fe8a7b14
Vishnu Kumar Verma <blueunova2003@gmail.com>
2024-02-23 11:15:09 +0530

    deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import DataItems from "./Components/DataItems";
 import Header from "./Components/Header";
 
 function App() {
+
+  // const apiKey = process.env.REACT_APP_API_KEY;
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.mainClass}>
